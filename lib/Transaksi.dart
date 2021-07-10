c1f4c07d5420542eaeee62109b3daf559403c3ba
artawiguna <57532508+artawiguna@users.noreply.github.com>
2021-07-10 11:53:27 +0800

    menampilkandata #3

--- lib/Transaksi.dart
+++ lib/Transaksi.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import './main.dart' as HomeUtama;
 import 'home.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp();
   runApp(Transaksi());
 }
 
-class Transaksi extends StatelessWidget {
+class Transaksi extends StatefulWidget {
+  const Transaksi({ Key key }) : super(key: key);
+
+  @override
+  _TransaksiState createState() => _TransaksiState();
+}
+
+class _TransaksiState extends State<Transaksi> {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  CollectionReference order = firestore.collection("order");
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -16,9 +28,68 @@ class Transaksi extends StatelessWidget {
           "Transaksi",
         ),
       ),
-      body: Center(
-        child:
-        new Text("Belum ada transaksi hari ini", style: TextStyle( fontSize : 20,)),)
+          body : Container(
+            width: MediaQuery.of(context).size.width,
+            child: StreamBuilder(
+              stream: Firestore.instance.collection("order").snapshots(),
+              builder: (context, snapshots),set()
+                return order(order: snapshot.data.document,);
+            ),
+
+          ),
     );
-    }
+  }
+
+  Set<Center> set() {
+    return {
+              if(!snapshot.hasData)
+                return Center(child: CircularProgressIndicator(),)
+            };
+  }
+}
+
+class Transakasi extends StatelessWidget {
+  const Transakasi({ Key key, this.order }) : super(key: key);
+  final List<DocumentSnapshot> order;
+  
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: order == null ? 0 : order.length,
+      itemBuilder: (context, i){
+        String masalah = order[i].data['masalah'].toString();
+        String lokasi = order[i].data["lokasi"].toString();
+        return ListTile(
+          title: Row(
+            children: <Widget> [
+              Text("Pemasalahan : $masalah"),
+              Text("lokasi: $lokasi"),
+            ],
+          ),
+        );
+      },
+      
+    );
+  }
+}
+
+/*class Transaksi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.blue,
+          title: new Text(
+            "Transaksi",
+          ),
+        ),
+        body: Center(
+          child: new Text("Belum ada transaksi hari ini",
+              style: TextStyle(
+                fontSize: 20,
+              )),
+        ));
+  }
 }
